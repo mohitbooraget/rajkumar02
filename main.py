b464f3001eb76459ff843d8a98c554a423a0a8b3
Manishdudi21 <160268803+Manishdudi21@users.noreply.github.com>
2024-03-28 20:41:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "7189525303:AAFq_cru4eSVj7p7EcatYAiekCBSkLSL2tQ",
+             bot_token= "7034221624:AAFElD8zRjrkBjiwqOp8c9cfeTNhIGfWA7E",
              api_id= 29611384,
              api_hash= "3090026bd04c23797e0c6ca1d563b5ec")
 ADMINS = [6585878012,-1002122540539]
